2992072801966175f7e3954a5a47326dff7146ab
samAricha <samaricha001@gmail.com>
2023-11-09 22:50:44 +0300

    cleaning catalog and normal dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,5 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("dagger.hilt.android.plugin")
     kotlin("kapt")
     alias(libs.plugins.androidApplication)
@@ -53,30 +51,29 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.coreKtx)
+    implementation(libs.androidx.appcompat)
+//    implementation(libs.android.material)
+    //material 3
+    implementation(libs.material3)
 
-    implementation("androidx.core:core-ktx:1.9.0")
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-//    implementation("androidx.appcompat:appcompat:1.6.1")
-//    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.compose.material3:material3")
+//    implementation("androidx.appcompat:appcompat:1.6.1")#removed when moving to compose
 
-    testImplementation("junit :junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     //Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.45")
     kapt("androidx.hilt:hilt-compiler:1.1.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-    implementation(libs.androidx.coreKtx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.android.material)
+
+
 
     testImplementation(libs.junit4)
     androidTestImplementation(libs.androidx.test.junit)
